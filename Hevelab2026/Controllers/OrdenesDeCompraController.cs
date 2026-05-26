865250ff8f92f566a02af7064583ff8b87f5142b
Angel <ander0villa@gmail.com>
2026-05-26 12:08:30 -0500

    Avance

---
+++ Hevelab2026/Controllers/OrdenesDeCompraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hevelab2026.Controllers
+{
+    public class OrdenesDeCompraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hevelab2026/Controllers/ProveedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hevelab2026.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
